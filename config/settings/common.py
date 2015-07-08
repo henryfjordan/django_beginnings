1bede2655807e06016bf1629cba005c0b9d4378d
Henry Jordan <henry.jordan@zefr.com>
2015-07-08 12:14:25 -0700

    added blog app and blog model

--- config/settings/common.py
+++ config/settings/common.py
@@ -45,6 +45,7 @@ THIRD_PARTY_APPS = (
 LOCAL_APPS = (
     'django_beginnings.users',  # custom users app
     # Your stuff: custom apps go here
+    'django_beginnings.blog',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
---
+++ django_beginnings/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django_beginnings.users.models import User
+
+# Create your models here.
+
+class BlogPost(models.Model):
+
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=500)
+    text = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
